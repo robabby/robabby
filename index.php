13e069f2dc169950265c82d1a37bc8b6af382bd4
Rob Abby <robabby23@gmail.com>
2012-09-22 13:08:37 -0500

    Updated Home Page Layout

--- index.php
+++ index.php
@@ -7,10 +7,10 @@
   //require_once("$path2root/assets/inc/connection.inc.php");
   //database connection info
   //$conn = mysql_connect('rawdesigns.db.7625389.hostedresource.com','rawdesigns','Forever#23') or trigger_error("SQL", E_USER_ERROR);
-  //$conn = mysql_connect('localhost','root','') or trigger_error("SQL", E_USER_ERROR);
-  //$db = mysql_select_db('rawdesigns',$conn) or trigger_error("SQL", E_USER_ERROR);
-  //$sql = "SELECT title, article, created, article_id FROM blog ORDER BY created DESC LIMIT 1";
-  //$result = mysql_query($sql, $conn) or trigger_error("SQL", E_USER_ERROR);
+  $conn = mysql_connect('localhost','root','') or trigger_error("SQL", E_USER_ERROR);
+  $db = mysql_select_db('rawdesigns',$conn) or trigger_error("SQL", E_USER_ERROR);
+  $sql = "SELECT title, article, created, article_id FROM blog ORDER BY created DESC LIMIT 3";
+  $result = mysql_query($sql, $conn) or trigger_error("SQL", E_USER_ERROR);
 ?>
 <!doctype html>
 <html>
@@ -78,143 +78,11 @@
     <div class="span6 textalign-right">
       <div class="well">
         <h3>A Preamble of Sorts</h3>
-        <p>As an avid internet enthusiast and web development artist for almost a decade, I offer a variety of web and technical services to a versatile client base. Working as a Freelancer in the Chicagoland area, I have formed a robust network of clientele with various businesses and individuals. From web development to graphic design, I can offer a comprehensive image for my clients to attract new customers, enthusiasts or marketplaces.</p>
+        <p>As an avid internet enthusiast and web development artist for almost a decade, I offer a variety of web and technical services to a versatile client base. Working as a Freelancer in the Chicagoland area, I have formed a robust network of clientele with various businesses and individuals. From web development to graphic design, I can offer a comprehensive image for my clients to attract new customers.</p>
       </div><!-- .well -->
     </div><!-- .span6 -->
-  </div><!-- .row -->
-  <br />
-  <br />
-  <h2 id="work">Current Employer <span>&darr;</span></h2>
-  <br />
-
-  <div class="row">
-    <div class="span12 work">
-      <h3><a href="http://web2carz.com" title="Web2Carz.com">Web2Carz.com</a></h3>
-      <a href="http://web2carz.com" title="Web2Carz.com">
-        <img class="thumbnail" src="/images/work/web2carz.jpg" />
-      </a>
-    </div><!-- .span -->
-  </div><!-- .row -->
-  <br />
-  <h2>Previous employment <span>&darr;</span></h2>
-  <br />
-
-  <div class="row">
-    <div class="span6 work">
-      <h3><a href="http://chicagobooth.edu" title="University of Chicago Booth">University of Chicago Booth</a></h3>
-      <a href="http://chicagobooth.edu" title="University of Chicago Booth">
-        <img class="thumbnail" src="/images/work/booth.jpg" />
-      </a>
-    </div><!-- .span -->
-    <div class="span6 work">
-      <h3><a href="http://www.artversion.com/" title="ArtVersion Interactive Agency">ArtVersion Interactive Agency</a></h3>
-      <a href="http://www.artversion.com/" title="ArtVersion Interactive Agency">
-        <img class="thumbnail" src="/images/work/artversion.jpg" />
-      </a>
-    </div><!-- .span -->
-  </div><!-- .row -->
-  <br />
-  <h2>Freelance work <span>&darr;</span></h2>
-  <br />
-  <div class="row">  
-
-    <div class="span4">
-      <div class="work">
-        <h3><a href="http://mmconsultants-inc.com/" title="MMC Inc.">Management Meeting Consultants</a></h3>
-        <a href="http://mmconsultants-inc.com/" title="MMC Inc.">
-          <img class="thumbnail" src="/images/work/mmc.jpg" width="286" />
-        </a>
-      </div><!-- .work -->
-    </div><!-- span -->
-
-    <div class="span4">
-      <div class="work">
-        <h3><a href="http://545north.com/" title="545 North Bar &amp; Grill">545 North</a></h3>
-        <a href="http://545north.com/" title="545 North Bar &amp; Grill">
-          <img class="thumbnail" src="/images/work/545.jpg" width="286" />
-        </a>
-      </div><!-- .work -->
-    </div><!-- span -->
-
-    <div class="span4">
-      <div class="work">
-        <h3><a href="http://danielasloan.com/" title="Daniela Sloan Acoustic Soul">Acoustic Soul</a></h3>
-        <a href="http://danielasloan.com/" title="Daniela Sloan Acoustic Soul">
-          <img class="thumbnail" src="/images/work/daniela.jpg" width="286" />
-        </a>
-      </div><!-- .work -->
-    </div><!-- span -->
-
-    
-    <div class="span12">
-      <br />
-      <br />
-      <h2>Technologies <span>&darr;</span></h2>
-      <br />
-      <br />
-    </div>
-    <div class="span6">
-      <h3>HTML5</h3>
-      <div class="progress progress-striped active">
-        <div class="bar" style="width: 90%;"></div>
-      </div>
-      <h3>jQuery</h3>
-      <div class="progress progress-striped active">
-        <div class="bar" style="width: 75%;"></div>
-      </div>
-      <h3>PHP</h3>
-      <div class="progress progress-striped active">
-        <div class="bar" style="width: 45%;"></div>
-      </div>
-      <h3>Ruby on Rails</h3>
-      <div class="progress progress-striped active">
-        <div class="bar" style="width: 15%;"></div>
-      </div>
-    </div>
-    <div class="span6">
-      <h3>CSS3</h3>
-      <div class="progress progress-striped active">
-        <div class="bar" style="width: 92%;"></div>
-      </div>
-      <h3>AJAX</h3>
-      <div class="progress progress-striped active">
-        <div class="bar" style="width: 30%;"></div>
-      </div>
-      <h3>MySQL</h3>
-      <div class="progress progress-striped active">
-        <div class="bar" style="width: 40%;"></div>
-      </div>
-      <h3>XML</h3>
-      <div class="progress progress-striped active">
-        <div class="bar" style="width: 30%;"></div>
-      </div>
-    </div>
-
-    <div class="span12">
-      <br />
-      <br />
-      <br />
-      <div class="contact">
-        <h2>Want even more? <span>&darr;</span></h2>
-        <br />
-        <a href="/sandbox.php" class="btn btn-inverse btn-large" title="">View my Sandbox</a>
-        <br />
-        <br />
-        <br />
-        <br />
-        <h2>Like what you see? <span>&darr;</span></h2>
-        <br />
-        <a href="/contact.php" class="btn btn-primary btn-large" title="">Contact Me</a>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-      </div><!-- .contact -->
-    </div><!-- span -->
-    
-  </div><!-- .row -->
+  </div><!-- .row -->    
+  </div><!-- .container -->
 </div><!-- #container -->
 
 
