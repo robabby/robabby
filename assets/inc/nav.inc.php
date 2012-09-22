3794a770fb135991fa5a62e2543b12a9506438bc
Rob Abby <robabby23@gmail.com>
2012-09-22 16:09:27 -0500

    Updated to responsive nav-bar

--- assets/inc/nav.inc.php
+++ assets/inc/nav.inc.php
@@ -1,18 +1,27 @@
 <?php $currentPage = basename($_SERVER['SCRIPT_FILENAME']); ?>
 
   <header>
-    <nav>
-      <ul>
-        <li <?php if ($currentPage == 'index.php') {
-          echo 'class="active"';} ?>><a class="first" href="<?php echo $path2root ?>/index.php">Home</a></li>
-        <!--<li <?php if ($currentPage == 'about.php') {
-          echo 'class="active"';} ?>><a href="<?php echo $path2root ?>/about.php">About</a></li>-->
-        <li <?php if ($currentPage == 'sandbox.php') {
-          echo 'class="active"';} ?>><a href="<?php echo $path2root ?>/sandbox.php">Sandbox</a></li>
-          <li <?php if ($currentPage == 'blog.php') {
-          echo 'class="active"';} ?>><a href="<?php echo $path2root ?>/blog.php">Blog</a></li>
-        <li class="last <?php if ($currentPage == 'contact.php') {
-          echo 'active';} ?>"><a href="<?php echo $path2root ?>/contact.php">Contact</a></li>
-      </ul>
-    </nav>
+    <div class="navbar">
+      <div class="navbar-inner">
+      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a>
+        <div class="nav-collapse">
+          <ul class="nav pull-right">
+            <li <?php if ($currentPage == 'index.php') {
+              echo 'class="active"';} ?>><a class="first" href="<?php echo $path2root ?>/index.php">Home</a></li>
+            <!--<li <?php if ($currentPage == 'about.php') {
+              echo 'class="active"';} ?>><a href="<?php echo $path2root ?>/about.php">About</a></li>-->
+            <li <?php if ($currentPage == 'sandbox.php') {
+              echo 'class="active"';} ?>><a href="<?php echo $path2root ?>/sandbox.php">Sandbox</a></li>
+              <li <?php if ($currentPage == 'blog.php') {
+              echo 'class="active"';} ?>><a href="<?php echo $path2root ?>/blog.php">Blog</a></li>
+            <li class="last <?php if ($currentPage == 'contact.php') {
+              echo 'active';} ?>"><a href="<?php echo $path2root ?>/contact.php">Contact</a></li>
+          </ul>
+        </div><!-- .nav-collapse -->
+      </div><!-- .nav-inner -->
+    </div><!-- .navbar -->
   </header>
