126cc653376c3a5dd9e8390228cf05741284b3dd
stat30fbliss <robabby23@gmail.com>
2013-01-23 12:47:17 -0600

    Modified footer dropup

--- assets/inc/footer.inc.php
+++ assets/inc/footer.inc.php
@@ -3,11 +3,7 @@
     <div class="btn-group dropup">
       <button class="btn primary dropdown-toggle" data-toggle="dropdown"><img src="/images/ui/gear2.png" /></button>
       <ul class="dropdown-menu">
-        <li><a data-toggle="modal" href="#contact">Feedback</a></li>
-        <li><a href="contact.php">Contact</a></li>
-        <li><a href="blog.php">Blog</a></li>
-        <li><a href="sandbox.php">Sandbox</a></li>
-        <li><a href="index.php">Home</a></li>
+        <li><a data-toggle="modal" href="#contact">Contact</a></li>
       </ul>
     </div>
     <div class="clearfix"></div>
