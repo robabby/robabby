b98438cc7628a3246e92d1f1c774e0bd768e7670
stat30fbliss <robabby23@gmail.com>
2013-01-23 11:43:25 -0600

    Removed passwords

--- assets/inc/connection.inc.php
+++ assets/inc/connection.inc.php
@@ -1,21 +1,24 @@
 <?php
 function dbConnect($usertype, $connectionType = 'mysqli') {
-  $host = 'rawdesigns.db.7625389.hostedresource.com';
-  //$host = 'localhost';  
-  $db = 'rawdesigns';
+  $host = 'localhost'; // The host address
+  $db = 'database_name'; // Name of the database
+
+  // For read-only
   if ($usertype  == 'read') {
-	$user = 'rawdesigns';
-	$pwd = 'Forever#23';
-  } elseif ($usertype == 'write') {
-  	$user = 'rawdesigns';
-	$pwd = 'Forever#23';
+  	$user = 'root';
+  	$pwd = '';
+  } 
+  // For read-write
+  elseif ($usertype == 'write') {
+  	$user = 'root';
+	  $pwd = '';
   } else {
-	exit('Unrecognized connection type');
+	  exit('Unrecognized connection type');
   }
   if ($connectionType == 'mysqli') {
-	$result = new mysqli($host, $user, $pwd, $db) ;
-	if (!$result) die ('Cannot connect to database');
-	return $result;
+	  $result = new mysqli($host, $user, $pwd, $db) ;
+	  if (!$result) die ('Cannot connect to database');
+	    return $result;
   } else {
     try {
       return new PDO("mysql:host=$host;dbname=$db", $user, $pwd);
