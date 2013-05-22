d094d774d1501b6bad358e78c41951243668691d
Rob <robabby23@gmail.com>
2013-05-22 16:56:41 -0500

    Cleanup

--- index.php
+++ index.php
@@ -2,11 +2,10 @@
   $path2root = $_SERVER['DOCUMENT_ROOT'];
   ob_start();
   try {
-  include('./assets/inc/title.inc.php'); 
-  include('./assets/inc/user_agent.php');
+  include('/assets/inc/title.inc.php'); 
+  include('/assets/inc/user_agent.php');
 ?>
 <!doctype html>
-<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
 <!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
 <!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
 <!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
