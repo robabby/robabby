2faf41f7e839c7ded54b6eb8376931fd99629b61
Rob <robabby23@gmail.com>
2013-05-22 19:28:30 -0500

    Modified IE Check

--- assets/inc/iecheck.inc.php
+++ assets/inc/iecheck.inc.php
@@ -1,18 +1,20 @@
-
-<!-- Prompt IE 6 users to install Chrome Frame. Remove this if you support IE 6.
-     chromium.org/developers/how-tos/chrome-frame-getting-started -->
 <!--[if lt IE 9]>
-<div class="container">
-  <div class="row">
-    <div class="span12">
-      <div class="well">
-        <h2>Woah! Your browser is ancient!</h2>
-        <h3>Its time to <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</h3>
-        <br />
-        <a class="btn btn-large btn-primary" href="http://browsehappy.com/">Browse Happy</a>
-        <a class="btn btn-large btn-info" href="http://www.google.com/chromeframe/?redirect=true">Google Chrome Frame</a>
-      </div>
-    </div>  
+<div class="modal" id="contact">
+  <div class="modal-header">
+    <a class="close" data-dismiss="modal">×</a>
+    <h3>Woah there!</h3>
   </div>
+  <div class="modal-body">
+    <h2>Your browser is ancient!</h2>
+    <p>Web Developers everywhere are urging people to stop using outdated web browsers.  Most specifically Internet Explorer.</p>
+    <p>Old web browsers make you more prone to malicious attacks, phishing attempts, and compromise your privacy.  Even more concerning is they seriously slow the advancement of the web by forcing developers to have to spend countless hours building products for aging, faulty software.</p>
+    <p>We also understand that you don't always have a choice in what browser you use.  If your organization forces you to use older versions of Internet Explorer, I urge you to install the Google Chrome Frame plug-in, which will leave your current browser intact while providing you with a much more enjoyable web browsing experince.</p>
+    <p>Give us a break, and update your browser today!</p>
+  </div>
+  <div class="modal-footer">
+    <a class="btn btn-large btn-primary" href="http://browsehappy.com/">Upgrade to a different browser</a>
+    <a class="btn btn-large btn-info" href="http://www.google.com/chromeframe/?redirect=true">Install Google Chrome Frame</a>
+  </div>
+  </form>
 </div>
 <![endif]-->
