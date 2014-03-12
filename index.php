7fb59779751b0031f2b2c08d9766df69ab222944
Rob Abby <robabby23@gmail.com>
2014-03-12 07:45:09 -0500

    Added in modal element with animations

--- index.php
+++ index.php
@@ -5,11 +5,7 @@
   include('./assets/inc/title.inc.php');
 ?>
 <!doctype html>
-<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8 ie7" lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js lt-ie9 ie8" lang="en"> <![endif]-->
-<!-- Consider adding a manifest.appcache: h5bp.com/d/Offline -->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<html lang="en">
 <head>
   <?php include('./assets/inc/head.inc.php'); ?>
 </head>
@@ -34,25 +30,25 @@
         </div>
         <nav id="social">
           <ul class="social-wrapper">
-            <li class="social-icon">
+            <li class="animated social-icon">
               <a href="#">
                 <i class="fa fa-twitter fa-5x"></i>
               </a>
             </li>
-            <li class="social-icon">
+            <li class="animated social-icon">
               <a href="#">
                 <i class="fa fa-linkedin-square fa-5x"></i>
               </a>
             </li>
-            <li class="social-icon">
+            <li class="animated social-icon">
               <a href="#">
                 <i class="fa fa-github fa-5x"></i>
               </a>
             </li>
           </ul>
         </nav>
-        <div class="bio">
-          
+        <div class="bio animated">
+          <a class="close" href="javascript:void(0)">x</a>
         </div>
       </div>
     </section>
@@ -61,8 +57,28 @@
     (function($) {
       var $profileImg = $('.profile-img'),
           $info = $('.info'),
-          $moreInfo = $('.more-info');
+          $moreInfo = $('.more-info'),
+          $socialIcon = $('.social-icon'),
+          $bio = $('.bio'),
+          $close = $('.close'),
+          vanilla = true;
 
+      $bio.hide();
+
+      // Dont show the social icons until 1.5 seconds after the DOM has loaded
+      setInterval(function() {
+        $socialIcon.each(function (i) {
+          // store the item around for use in the 'timeout' function
+          var $item = $(this); 
+          // execute this function sometime later:
+          setTimeout(function() { 
+            $item.addClass('slideInLeft visible');
+          }, 500 * i);
+          // each element should animate half a second after the last one.
+        });
+      }, 1500);
+
+      // Flip the profile image on hover
       $profileImg.hover(function() {
         $this = $(this);
         $this.removeClass('bounceInUp');
@@ -71,9 +87,51 @@
 
       $moreInfo.on('click', function() {
         var $this = $(this);
-        $profileImg.removeClass('bounceInUp');
+        
+        if($profileImg.hasClass('bounceInDown')) {
+          $profileImg.removeClass('bounceInDown'); 
+        } else if ($profileImg.hasClass('flip')) {
+          $profileImg.removeClass('flip');
+        }
+
         $profileImg.addClass('bounceOutUp');
+
+        // Clean up the info element
+        if ($info.hasClass('bounceInUp')) {
+          $info.removeClass('bounceInUp');
+        }
+
+        // Animate out .info div
         $info.addClass('bounceOutDown');
+
+        // Clean up the .bio element
+        if ($bio.hasClass('flipOutX')) {
+          $bio.removeClass('flipOutX');
+        }
+
+        // Unhide the modal
+        $bio.show(); 
+
+        setInterval(function() {
+          $bio.addClass('flipInX visible');
+        }, 250);
+      });
+
+      $close.on('click', function(e) {
+        // Remove the .bio element from the view
+        $bio.addClass('flipOutX');
+
+        setInterval(function() {
+          // Clean up elements
+          $profileImg.removeClass('bounceOutUp');
+          $info.removeClass('bounceOutDown');
+
+          // Bring back the starting elements
+          $profileImg.addClass('bounceInDown');
+          $info.addClass('bounceInUp');
+
+          $bio.hide();
+        }, 500);
       });
     })(jQuery);
     </script>
