d26e9becc7c4479084f897ff319fd777614d6993
Rob Abby <robabby23@gmail.com>
2014-03-24 07:58:33 -0500

    Commented out auto-script-dump

--- assets/inc/footer.inc.php
+++ assets/inc/footer.inc.php
@@ -1,22 +1,20 @@
-<footer>
-  
-</footer>
-
 <?php
 // Collect all of the scripts and render them on the page
-if ($handle = opendir("$path2root/assets/js")) {
+//if ($handle = opendir("$path2root/assets/js")) {
 
     /* This is the correct way to loop over the directory. */
-    while (false !== ($entry = readdir($handle))) {
+//    while (false !== ($entry = readdir($handle))) {
       // only collect the necessary files
-        if (($entry != "." && $entry != ".." && $entry != "modernizr.custom.48780.js") && strpos($entry, ".js")) {
-            echo "<script src=\"/assets/js/".$entry."\"></script>\n";
-        }
-    }
+//        if (($entry != "." && $entry != ".." && $entry != "modernizr.custom.48780.js") && strpos($entry, ".js")) {
+//            echo "<script src=\"/assets/js/".$entry."\"></script>\n";
+//        }
+//    }
 
-    closedir($handle);
-}
+//    closedir($handle);
+//}
 ?>
+<script src="/assets/js/hide-address-bar.js"></script>
+<script src="/assets/js/plugins.js"></script>
 <script type="text/javascript">
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-32901659-1']);
