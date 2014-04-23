884f5f755cc2a9d6be9e45f4e1eddbd3898f6db0
Rob Abby <robabby23@gmail.com>
2014-04-22 19:01:30 -0500

    File cleanup

--- assets/inc/nav.inc.php
+++
@@ -1,5 +0,0 @@
-<?php $currentPage = basename($_SERVER['SCRIPT_FILENAME']); ?>
-<!--
-<div class="navbar navbar-fixed-top">
-  <a class="navbar-brand pull-right" href="/">Rob Abby</a>
-</div><!-- /.navbar -->
