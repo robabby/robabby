f89d1b1e142878ec63a594d9f2767f58567f8df7
Rob Abby <robabby23@gmail.com>
2015-02-16 21:09:13 -0600

    Installed textAngular editor

--- config/env/all.js
+++ config/env/all.js
@@ -15,6 +15,7 @@ module.exports = {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.css',
 				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+				'public/lib/textAngular/src/textAngular.css'
 			],
 			js: [
 				'public/lib/angular/angular.js',
@@ -26,7 +27,10 @@ module.exports = {
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
 				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
-				'public/lib/angular-truncate-original/src/truncate.js'
+				'public/lib/angular-truncate-original/src/truncate.js',
+				'public/lib/textAngular/dist/textAngular-rangy.min.js',
+				'public/lib/textAngular/dist/textAngular-sanitize.min.js',
+				'public/lib/textAngular/dist/textAngular.min.js'
 			]
 		},
 		css: [
