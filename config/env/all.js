a7e1342627f11a53213370466545b875915d4f6a
Rob Abby <robabby23@gmail.com>
2015-02-17 19:05:11 -0600

    Implemented textAngular on post create page

--- config/env/all.js
+++ config/env/all.js
@@ -15,6 +15,7 @@ module.exports = {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.css',
 				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+				'public/lib/font-awesome/css/font-awesome.min.css',
 				'public/lib/textAngular/src/textAngular.css'
 			],
 			js: [
