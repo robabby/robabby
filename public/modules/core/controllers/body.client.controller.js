8eced0031e87f78268588e45d8a5bbea218e6aad
Rob Abby <robabby23@gmail.com>
2015-02-20 15:50:44 -0600

    Implemented header show/hide logic

--- public/modules/core/controllers/body.client.controller.js
+++ public/modules/core/controllers/body.client.controller.js
@@ -4,6 +4,11 @@ angular.module('core').controller('BodyController', ['$scope', 'Authentication',
   function($scope, Authentication) {
     $scope.authentication = Authentication;
     $scope.elementStates = {};
-    $scope.elementStates.topBarActive = true;
+
+    if($scope.authentication.user) {
+      $scope.elementStates.topBarActive = true;
+    } else {
+      $scope.elementStates.topBarActive = false;
+    }
   }
 ]);
