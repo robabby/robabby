2c4606a2d7d8bfd8011108da34067dfc01594307
Rob Abby <robabby23@gmail.com>
2015-02-19 19:12:30 -0600

    Created new BodyController

---
+++ public/modules/core/controllers/body.client.controller.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('core').controller('BodyController', ['$scope', 'Authentication',
+  function($scope, Authentication) {
+    $scope.authentication = Authentication;
+    $scope.elementStates = {};
+    $scope.elementStates.topBarActive = false;
+  }
+]);
