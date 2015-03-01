6c18dd81110aec6595e7ac1128839fabb2572bab
Rob Abby <robabby23@gmail.com>
2015-02-28 23:08:22 -0600

    Implemented nav toggle logic

--- public/modules/core/controllers/nav.client.controller.js
+++ public/modules/core/controllers/nav.client.controller.js
@@ -1,8 +1,23 @@
 'use strict';
 
-angular.module('core').controller('NavController', ['$scope',
-	function($scope) {
-		// Nav controller controller logic
-		// ...
+angular.module('core').controller('NavController', ['$scope', '$window',
+	function($scope, $window) {
+		
+		var $ = $window.jQuery;
+
+		$scope.isActive = false;
+
+		$scope.triggerNav = function() {
+			var $el = $('#global-nav'),
+					activeClass = 'is-open';
+
+			if(!$scope.isActive) {
+				$scope.isActive = true;
+				$el.addClass(activeClass);
+			} else {
+				$scope.isActive = false;
+				$el.removeClass(activeClass);
+			}
+		};
 	}
 ]);
