9f89c937d17e844cb8d8c4e13d3344f823249eb8
robabby <robabby23@gmail.com>
2016-01-14 07:39:58 -0600

    Updated blog route

--- client/app/routes/blog.js
+++ client/app/routes/blog.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-		return this.store.findAll('post');
+    var posts = this.store.findAll('post');
+		return posts;
 	}
 });
