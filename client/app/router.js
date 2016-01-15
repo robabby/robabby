66f5d4fe942deeea3d9f5d929b9fd76bfd56d508
robabby <robabby23@gmail.com>
2016-01-15 08:47:47 -0600

    Updated path and route for posts

--- client/app/router.js
+++ client/app/router.js
@@ -7,9 +7,8 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('home', { path: '/' });
-  this.route('blog', function() {
-    this.route('post', {path: '/:post_id'});
-  });
+  this.route('blog');
+  this.route('post', {path: 'blog/:post_id'});
   this.route('about');
 });
 
