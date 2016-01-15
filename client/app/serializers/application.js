25f7d2836b0982901b79525f8428d1e6c5569119
robabby <robabby23@gmail.com>
2016-01-14 21:25:49 -0600

    Changed to JSON serializer

--- client/app/serializers/application.js
+++ client/app/serializers/application.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
-export default DS.RESTSerializer.extend({
+export default DS.JSONSerializer.extend({
   primaryKey: '_id',
   serializeId: function(id) {
       return id.toString();
