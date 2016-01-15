330d6e9391a1b7105ee120457a96beed2dacd027
robabby <robabby23@gmail.com>
2016-01-14 21:25:29 -0600

    Changed to JSONAPI adapter

--- client/app/adapters/application.js
+++ client/app/adapters/application.js
@@ -1,6 +1,5 @@
 import DS from 'ember-data';
 
-export default DS.RESTAdapter.extend({
-  namespace: 'api',
-	host: 'http://localhost:4500'
+export default DS.JSONAPIAdapter.extend({
+  namespace: 'api'
 });
