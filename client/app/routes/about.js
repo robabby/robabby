fe37696c46e8a2b406c9565971e6ca6f6e9bb0b3
Rob Abby <robabby23@gmail.com>
2016-01-24 14:49:03 -0600

    Refactored side nav to header and created mixins

--- client/app/routes/about.js
+++ client/app/routes/about.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  activate: function() {
+    Ember.$('body').addClass('about');
+  },
+  deactivate: function() {
+    Ember.$('body').removeClass('about');
+  }
 });
