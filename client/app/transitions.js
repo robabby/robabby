8c29b58094805a7abec61c07705e15295dbeaf5b
robabby <robabby23@gmail.com>
2016-02-01 06:39:34 -0600

    Updated transitions

--- client/app/transitions.js
+++ client/app/transitions.js
@@ -2,7 +2,7 @@ export default function(){
   this.transition(
     this.fromRoute('home'),
     this.toRoute('about'),
-    this.use('fade'),
-    this.reverse('fade')
+    this.use('toUp'),
+    this.reverse('toDown')
   );
 }
