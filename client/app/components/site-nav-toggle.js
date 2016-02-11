a5efe4d0fbb1d6b48eaf61d89268312ecbdd00c2
robabby <robabby23@gmail.com>
2016-02-11 17:11:17 -0600

    Toggling service value

--- client/app/components/site-nav-toggle.js
+++ client/app/components/site-nav-toggle.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'a',
+  classNames: 'cd-nav-trigger',
+  siteNavState: Ember.inject.service('site-nav-state'),
+  click() {
+    var currentNavState = this.get('siteNavState');
+
+    if(currentNavState.isOpen) {
+      currentNavState.set('isOpen', false);
+    } else {
+      currentNavState.set('isOpen', true);
+    }
+  }
 });
