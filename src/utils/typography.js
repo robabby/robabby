121c12d149801cbb076db9396f37a1c6e8f6eedf
robabby <robabby23@gmail.com>
2017-12-23 00:25:56 -0600

    Changed theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,13 +1,8 @@
 import Typography from 'typography'
-import Wordpress2016 from 'typography-theme-wordpress-2016'
+// import GitHub from 'typography-theme-github'
+import Lawton from 'typography-theme-lawton'
 
-Wordpress2016.overrideThemeStyles = () => ({
-  'a.gatsby-resp-image-link': {
-    boxShadow: 'none',
-  },
-})
-
-const typography = new Typography(Wordpress2016)
+const typography = new Typography(Lawton)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
