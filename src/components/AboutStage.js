d2d570d2f3c4e252467f4654994d79a016777104
robabby <robabby23@gmail.com>
2017-12-23 21:47:33 -0600

    Animated Meditating Dude

--- src/components/AboutStage.js
+++ src/components/AboutStage.js
@@ -1,12 +1,19 @@
 import React from 'react'
 
+import meditatingDude from '../assets/images/meditating-dude.svg'
 import '../assets/scss/components/AboutStage.scss'
 
 class AboutStage extends React.Component {
   render() {
     return (
       <div className="ra-about-stage">
-        <h1 className="ra-about-stage__title">What about me?</h1>
+        <img
+          className="ra-about-stage__meditating-dude"
+          src={meditatingDude}
+          alt={`Meditating Dude`}
+        />
+        <div className="ra-about-stage__shadow"></div>
+        {/* <h1 className="ra-about-stage__title">What about me?</h1> */}
       </div>
     )
   }
