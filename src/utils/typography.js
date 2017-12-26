161fb025935dfa6110f7985d0415dba80b3f062d
robabby <robabby23@gmail.com>
2017-12-26 17:32:05 -0600

    Revised Typography theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,8 +1,18 @@
 import Typography from 'typography'
-import GitHub from 'typography-theme-github'
-import Lawton from 'typography-theme-lawton'
 
-const typography = new Typography(Lawton)
+import 'typeface-raleway'
+import 'typeface-roboto'
+import 'typeface-open-sans'
+
+const config = {
+  title: 'typography-theme-robabby',
+  scaleRatio: 2.5,
+  headerFontFamily: ['Raleway', 'sans-serif'],
+  headerWeight: 800,
+  bodyFontFamily: ['Open Sans', 'sans-serif']
+}
+
+const typography = new Typography(config)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
