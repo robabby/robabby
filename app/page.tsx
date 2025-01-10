06db36638a9bd8afa6223e1aee0c1766c5e63eb4
Rob Abby <robabby23@gmail.com>
2025-01-10 13:26:43 -0600

    Cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Header from './components/Header'
+// import Header from './components/Header'
 import Hero from './components/Hero'
 import About from './components/About'
 import Skills from './components/Skills'
 import Experience from './components/Experience'
-import Contact from './components/Contact'
+// import Contact from './components/Contact'
 import Footer from './components/Footer'
 
 export default function Home() {
