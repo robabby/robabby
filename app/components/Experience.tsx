c32969bc8db345e5bb8d73feca22cbb1cee0744f
Rob Abby <robabby@proton.me>
2025-07-25 20:41:16 -0400

    Added companyLink property

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -3,6 +3,7 @@ export default function Experience() {
     {
       title: "Staff Frontend Engineer",
       company: "PartySlate",
+      companyLink: "https://www.partyslate.com",
       period: "2018 - 2024",
       description: "PartySlate (B2C) is a photo-rich platform & dual-sided marketplace that connects people planning events with leading vendors and venues. In my time at PartySlate we grew from a 12-person early-stage startup to a +50-employee Series-B company while averaging 258.33% revenue growth year-over-year.",
       highlights: [
@@ -14,6 +15,7 @@ export default function Experience() {
     {
       title: "Senior UX Engineer",
       company: "project44",
+      companyLink: "https://www.project44.com",
       period: "2018 - 2018",
       description: "project44 (B2B) is the leading Decision Intelligence Platform for supply chains, powered by the world's largest transportation data network. I worked within the Product Experience team defining and developing processes and features for project44’s new User Interface products, sitting atop their existing API based solutions.",
       highlights: [
@@ -26,6 +28,7 @@ export default function Experience() {
     {
       title: "Senior Product Developer",
       company: "Savo",
+      companyLink: "https://pitchbook.com/profiles/company/13234-69#overview",
       period: "2013 - 2018",
       description: "SAVO (B2B) was an industry leader in the sales enablement space, providing SaaS software used by some of the largest companies in the world (Disney, American Express, Google, UPS, etc.). I worked within Agile and Lean UX methodologies taking projects from discovery to implementation.",
       highlights: [
@@ -37,6 +40,7 @@ export default function Experience() {
     {
       title: "Web Developer",
       company: "Web2Carz",
+      companyLink: "https://www.web2carz.com",
       period: "2011 - 2013",
       description: "Web2Carz (B2C) is a used car marketplace and editorial platform. I was hired as the first developer and worked directly with the founders to implement a major redesign of the Web2Carz aging site, implement a new editorial segment, and develop an adaptive mobile solution.",
       highlights: [
@@ -54,7 +58,7 @@ export default function Experience() {
           {experiences.map((exp, index) => (
             <div key={index} className="bg-gray-100 p-6 rounded-lg shadow-md hover:shadow-lg transition duration-300">
               <h3 className="text-xl font-semibold mb-2">{exp.title}</h3>
-              <p className="text-blue-600 mb-2">{exp.company}</p>
+              <a className="text-blue-600 mb-2" href={exp.companyLink} target="_blank">{exp.company}</a>
               <p className="text-gray-600 mb-4">{exp.period}</p>
               <p className="text-gray-700 mb-4">{exp.description}</p>
               <ul className="text-gray-700 list-disc list-inside space-y-1">
