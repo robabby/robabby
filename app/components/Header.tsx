35fc0396c140e1d801de8d5f1111f0dc51b62df6
Rob Abby <robabby@proton.me>
2025-07-25 20:25:52 -0400

    Mobile enhancements

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 bg-white shadow-md">
+    <header className="fixed md:top-0 bottom-0 md:bottom-auto left-0 right-0 z-50 bg-white shadow-md">
       <nav className="container mx-auto px-6 py-3">
         <ul className="flex justify-center space-x-8">
           <li><Link href="#about" className="text-gray-800 hover:text-blue-600 transition duration-300">About</Link></li>
