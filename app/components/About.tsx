b2fd7e6f974cf06981d1aee39fc4a95412a031bd
Rob Abby <robabby@proton.me>
2025-09-24 15:48:47 -0400

    Updated About

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -8,7 +8,7 @@ export default function About() {
           I have 15 years of experience building performant, user-centered web applications across B2B and B2C domains 
           in agile software development teams. I specialize in React frameworks and excel at translating complex technical 
           requirements into scalable UI systems. I bring deep experience with design systems, component architecture, 
-          refactoring and cross-functional collaboration—and I'm seeking frontend software engineering roles where thoughtful 
+          refactoring and cross-functional collaboration—and I&apos;m seeking frontend software engineering roles where thoughtful 
           code meets innovation.
         </p>
         </div>
