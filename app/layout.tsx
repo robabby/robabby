7eb60b52828bb9e5c8b29bf4c99f6934f9761095
Rob Abby <robabby@proton.me>
2025-09-27 15:04:18 -0400

    Cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,11 @@ import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Theme } from "@radix-ui/themes";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: 'Rob Abby - Product Engineer',
-  description: 'Personal website of Rob Abby, a Product Engineer specializing in creating exceptional web experiences.',
+  title: "Rob Abby - Product Engineer",
+  description: "Personal website of Rob Abby, a Product Engineer specializing in creating exceptional web experiences.",
 }
 
 export default function RootLayout({
