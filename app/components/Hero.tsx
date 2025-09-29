4a9f59ad46c093981364b84d56f8002bf7c2ec56
Rob Abby <robabby@proton.me>
2025-09-29 16:22:34 -0400

    Updated Metatron's Cube

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,13 +1,17 @@
+"use client";
+
 import { Box, Button, Container, Flex, Heading, IconButton, Link, Section, Text } from "@radix-ui/themes";
 import { EnvelopeClosedIcon, GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
 import { FileIcon } from "@radix-ui/react-icons";
 import MetatronsCube from "./MetatronsCube";
+import "./Hero.css";
 
 export default function Hero() {
   return (
     <Section id="home" style={{ background: "var(--blue-a9)", position: "relative" }}>
       <Box py="9">
-        <Container py="8" size="3">
+        <MetatronsCube />
+        <Container py="8" size="3" style={{ position: "relative", zIndex: 1 }}>
           <Flex direction="column" gap="4" align="center">
             <Heading mb="4" size="9">Robert Abby</Heading>
             <Text mb="4" size="7">Product Engineer</Text>
@@ -36,7 +40,6 @@ export default function Hero() {
             </Button>
           </Flex>
         </Container>
-        <MetatronsCube />
       </Box>
     </Section>
   )
